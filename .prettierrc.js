950d914175ed63a0d487961798b41d8c18e433e7
mania605 <kim.minsun83@gmail.com>
2024-09-12 11:37:03 +0900

    다양한 조건문 형태

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+//module.exports = {
+"printWidth": 140, //몇글자까지 줄바꿈 하지 않고 옆으로 나열할지 설정
+	"useTabs": true, //탭기 눌렀을때 자동완성기능 사용 여부 
+		"semi": true, //코드 입력후 자동으로 구문 뒤에 ; (세미콜론 입력 여부)
+			"singleQuote": false, // true(작은따옴표 변환) / false(큰 따옴표 변환)
+				"trailingComma": "none", //'none' (객체 표기시 마지막 프로퍼티 뒤에 콤마 자동 제거)
+					"bracketSpacing": true, //true(괄호사이에 한칸씩 띄우기) / false (괄호사이에 한칸씩 띄우지 않기)
+						"arrowParens": "avoid", //"avoid" 화살표 함수작성시 파라미터가 1개일때 괄호 생략하기
+							"singleAttributePerLine": false // true(요소에 속성이 여러개일때 속성마다 줄바꿈 ) / false (줄바꿈하지 않기)
+//};
--- script.js
+++ script.js
@@ -15,5 +15,39 @@ else if(조건식2){
 else {
 위의 모든 조건이 참이 아닐때 이곳의 구문이 실행 되고 종료 
 }
-
 */
+
+/* 시간대별 색갈 다르게 적용
+0-9 :lightblue
+10-15: orange
+16-20: pink
+21-24: purple
+
+const hour = new Date().getUTCHours();  // 현재 시간을 가져옴
+console.log(hour);
+
+if (hour >= 0 && hour < 10) {
+    document.body.style.backgroundColor = "lightblue";
+} else if (hour >= 10 && hour < 16) {
+    document.body.style.backgroundColor = "orange";
+} else if (hour >= 16 && hour < 21) {
+    document.body.style.backgroundColor = "pink";
+} else {
+    document.body.style.backgroundColor = "purple";
+}
+
+console.log(document.body.style.backgroundColor);  // 현재 배경색 확인
+*/
+const hour = new Date().getHours();  // 현재 시간을 가져옴
+if (hour >= 0 && hour < 10) document.body.style.backgroundColor = "lightblue";
+if (hour >= 10 && hour < 16) document.body.style.backgroundColor = "orange";
+if (hour >= 16 && hour < 21) document.body.style.backgroundColor = "pink";
+if (hour >= 21 && hour < 24) document.body.style.backgroundColor = "purple";
+
+
+//삼항연산자 
+//위에 없으면 넣고 const hour = new Date().getHours();
+document.body.style.backgroundColor = hour < 10 ? "lightblue" :
+    hour < 16 ? "orange" :
+        hour < 21 ? "pink" : "purple"
+
