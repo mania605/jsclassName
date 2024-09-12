51b889e3fedc48fd5096f00a2f9af1c8a2d59a7f
mania605 <kim.minsun83@gmail.com>
2024-09-12 17:33:19 +0900

    가운데 짝대기 모션처리

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
-const btnReset = document.querySelectorAll("button")[0];
-const btnActive = document.querySelectorAll("button")[1];
-const btnToggle = document.querySelectorAll("button")[2];
-const box = document.querySelector("#box");
+const btnToggle = document.querySelector(".btnToggle");
+const [topEL, middleEL, bottomEl] = btnToggle.querySelectorAll("div");
 
-/*
-DomEl.classList.add('클래스명') 요소에 클래스명 추가
-DomEl.classList.remove('클래스명') 요소에 해당 클래스명 제거
-DomEl.classList.remove() 요소에 있는 모든 클래스명 제거
-DomEl.classList.toggle('클래스명') 해당클래스명이 있으면 제거하고 없으면 추가
-DomEl.classList.contains('클래스명') 해당 클래스명이 있으면 True반환 없으면 false반환
-*/
-
-btnActive.addEventListener('click', () => {
-  box.classList.add("on");
-});
-
-btnReset.addEventListener('click', () => {
-  box.classList.remove("on");
-});
-
-btnToggle.addEventListener('click', () => {
-  box.classList.toggle("on");
-});
+btnToggle.addEventListener("click", () => {
+  btnToggle.classList.toggle("on");
+})
