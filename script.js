5544f188a788d5f3ba717dbbf02a65e3c5799d64
mania605 <kim.minsun83@gmail.com>
2024-09-13 09:30:04 +0900

    9일차 모바일패널추가

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const btnToggle = document.querySelector(".btnToggle");
-const [topEL, middleEL, bottomEl] = btnToggle.querySelectorAll("div");
 
 btnToggle.addEventListener("click", () => {
   btnToggle.classList.toggle("on");
-})
+});
