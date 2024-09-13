8dfe5f79addda5f102b4f085e9c5da5dc90cd4f5
mania605 <kim.minsun83@gmail.com>
2024-09-13 10:04:41 +0900

    모바일패널

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const btnToggle = document.querySelector(".btnToggle");
+const mibilePanel = document.querySelector('.mobilePanel');
 
 btnToggle.addEventListener("click", () => {
-  btnToggle.classList.toggle("on");
+  //  btnToggle.classList.toggle("on");
+  //  mobilePanel.classList.toggle("on");
+  [btnToggle, mobilePanel], forEach(el => el.classList.toggle('on'));
 });
